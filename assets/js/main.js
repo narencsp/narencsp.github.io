59b814f0eb7fff911dabf34e2a8f673b95b94144
Narendran <47205680+narencsp@users.noreply.github.com>
2020-09-01 01:31:47 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,7 +63,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "narencsp");
+     GitHubCalendar("#github-graph", "narencsp", { responsive: true });
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity 
